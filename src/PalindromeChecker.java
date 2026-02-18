8c43f3528acd197e625d5f60992e6e93b7cb338b
anusharoy24 <anusharc24@gmail.com>
2026-02-18 10:31:28 +0530

    UC1: printing statment

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
         System.out.println("Welcome");
+        System.out.println("version :1.0");
+        System.out.println("System initialised successfully");
     }
 }
